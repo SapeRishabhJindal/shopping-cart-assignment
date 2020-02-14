885717a7f244aceac3ae28709e17ee1d1dcdba30
Rishabh Jindal <risjinda@publicisgroupe.net>
2020-02-14 12:09:41 +0530

    signin and register page

--- src/app/templates/register/register.component.ts
+++ src/app/templates/register/register.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['../signin/signin.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
